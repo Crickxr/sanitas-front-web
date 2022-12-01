44f5d1c49a87dc6fc09a364a2b8d092fb2739e90
Enric Colomer <enric.colomer.mur@gmail.com>
2022-12-01 15:16:35 +0100

    unit testing

--- test_app/src/app/components/login-card/login-card.component.spec.ts
+++ test_app/src/app/components/login-card/login-card.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { LoginCardComponent } from './login-card.component';
 
@@ -20,4 +21,30 @@ describe('LoginCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('error variable should be false by default', () => {
+    expect(component.error).toBeFalse();
+  });
+
+  it('formGroup should be defined', () => {
+    expect(component.formGroup).toBeDefined();
+  });
+
+  it('formGroup should be defined', () => {
+    expect(component.shakeOnError).toBeDefined();
+  });
+
+  it('shakeOnError should equal to default by default', () => {
+    expect(component.shakeOnError).toEqual('default');
+  });
+
+  it('user should update from form changes', (() => {
+    const testUser = {
+      email: 'test@test.com',
+      password: '12345'
+    };
+    component.formGroup.controls['email'].setValue(testUser.email);
+    component.formGroup.controls['password'].setValue(testUser.password);
+    expect(component.formGroup.valid).toBeTrue();
+  }));
 });
