f7534109dcb21d58b1d0738f32874aee35dd9e90
Enric Colomer <enric.colomer.mur@gmail.com>
2022-12-01 10:10:45 +0100

    dinamic form logic installed

--- test_app/src/app/app.component.ts
+++ test_app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test_app';
+
+  formGroup: FormGroup;
+  error: boolean = false;
+
+  constructor (private formBuilder: FormBuilder) {
+    
+  
+    var el = document.getElementsByClassName('mat-mdc-text-field-wrapper');
+    // el.style = 'background-color: white !important;';
+    console.log(el);
+    this.formGroup = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.min(5)]),
+      remember: new FormControl(false, []),
+    },{updateOn:'submit'});
+
+    // setInterval(() => { console.log(this.formGroup.value) }, 2000);
+  }
+  
+
+  onClickAcces() {
+    (this.formGroup.valid) ? console.log('OK'): this.error = true;
+  }
+  
 }
